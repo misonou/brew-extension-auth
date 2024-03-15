998eac028c5403523f95d12ba6bdd8d17df90d90
Takeru <takeru.moe@gmail.com>
2024-03-15 13:06:16 +0800

    0.2.0

---
+++ dist/brew-auth-msal.js
@@ -0,0 +1,264 @@
+/*! @misonou/brew-extension-auth v0.2.0 | (c) misonou | https://misonou.github.io */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("@azure/msal-browser"), require("brew-js"), require("zeta-dom"));
+	else if(typeof define === 'function' && define.amd)
+		define("@misonou/brew-extension-auth/msal", ["@azure/msal-browser", "brew-js", "zeta-dom"], factory);
+	else if(typeof exports === 'object')
+		exports["MsalAuthProvider"] = factory(require("@azure/msal-browser"), require("brew-js"), require("zeta-dom"));
+	else
+		root["brew"] = root["brew"] || {}, root["brew"]["Auth"] = root["brew"]["Auth"] || {}, root["brew"]["Auth"]["MsalAuthProvider"] = factory(root["msal"], root["brew"], root["zeta"]);
+})(self, function(__WEBPACK_EXTERNAL_MODULE__735__, __WEBPACK_EXTERNAL_MODULE__760__, __WEBPACK_EXTERNAL_MODULE__231__) {
+return /******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 735:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__735__;
+
+/***/ }),
+
+/***/ 760:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__760__;
+
+/***/ }),
+
+/***/ 231:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__231__;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+!function() {
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": function() { return /* binding */ msal; }
+});
+
+// EXTERNAL MODULE: external {"commonjs":"@azure/msal-browser","commonjs2":"@azure/msal-browser","amd":"@azure/msal-browser","root":"msal"}
+var msal_browser_root_msal_ = __webpack_require__(735);
+// EXTERNAL MODULE: external {"commonjs":"brew-js","commonjs2":"brew-js","amd":"brew-js","root":"brew"}
+var external_commonjs_brew_js_commonjs2_brew_js_amd_brew_js_root_brew_ = __webpack_require__(760);
+;// CONCATENATED MODULE: ./|umd|/brew-js/util/common.js
+
+var getJSON = external_commonjs_brew_js_commonjs2_brew_js_amd_brew_js_root_brew_.getJSON;
+
+;// CONCATENATED MODULE: ./|umd|/brew-js/util/path.js
+
+var isSubPathOf = external_commonjs_brew_js_commonjs2_brew_js_amd_brew_js_root_brew_.isSubPathOf;
+
+// EXTERNAL MODULE: external {"commonjs":"zeta-dom","commonjs2":"zeta-dom","amd":"zeta-dom","root":"zeta"}
+var external_commonjs_zeta_dom_commonjs2_zeta_dom_amd_zeta_dom_root_zeta_ = __webpack_require__(231);
+;// CONCATENATED MODULE: ./|umd|/zeta-dom/util.js
+
+var _lib$util = external_commonjs_zeta_dom_commonjs2_zeta_dom_amd_zeta_dom_root_zeta_.util,
+  util_define = _lib$util.define,
+  extend = _lib$util.extend,
+  is = _lib$util.is;
+
+;// CONCATENATED MODULE: ./src/msal.js
+
+
+
+
+var AUTHORITY_URI = 'https://login.microsoftonline.com/';
+var MsalAuthProvider = {};
+var issuerCache = {};
+var multitenant = {
+  common: true,
+  organizations: true
+};
+var defaultConfig = {
+  cache: {
+    cacheLocation: 'localStorage'
+  }
+};
+function clearInteractionStatus() {
+  delete sessionStorage['msal.interaction.status'];
+}
+function getIssuerURL(domain) {
+  return issuerCache[domain] || (issuerCache[domain] = getJSON(AUTHORITY_URI + domain + '/v2.0/.well-known/openid-configuration').then(function (v) {
+    return v.issuer;
+  }, function (xhr) {
+    if (xhr.status === 400) {
+      return '';
+    }
+    delete issuerCache[domain];
+  }));
+}
+
+/**
+ * @param {string} key
+ * @param {PublicClientApplication} client
+ * @param {*} options
+ */
+function createProvider(key, client, options) {
+  var scopes = options.scopes;
+  function refresh(current, context) {
+    return client.acquireTokenSilent({
+      redirectUri: context.redirectUri,
+      account: current.account,
+      scopes: scopes
+    });
+  }
+  function handleResult(result) {
+    if (result) {
+      var account = result.account;
+      client.setActiveAccount(account);
+      return {
+        account: account,
+        accountId: account.homeAccountId,
+        accessToken: result.accessToken,
+        expiresOn: result.expiresOn
+      };
+    }
+  }
+  return {
+    key: key,
+    authType: 'federated',
+    providerType: 'msal',
+    refresh: refresh,
+    isHandleable: function isHandleable(loginHint) {
+      if (!/@(.+)$/.test(loginHint)) {
+        return false;
+      }
+      var authority = client.getConfiguration().auth.authority;
+      if (multitenant[authority.slice(AUTHORITY_URI.length)]) {
+        return true;
+      }
+      return getIssuerURL(RegExp.$1).then(function (issuer) {
+        return !!isSubPathOf(issuer, authority);
+      });
+    },
+    init: function init(context) {
+      client.enableAccountStorageEvents();
+      client.addEventCallback(function (message) {
+        if (message.eventType === msal_browser_root_msal_.EventType.ACCOUNT_REMOVED) {
+          context.revokeSession(message.payload.homeAccountId);
+        }
+      });
+      return client.initialize().then(function () {
+        var account = client.getActiveAccount();
+        return account ? refresh({
+          account: account
+        }, context) : client.handleRedirectPromise();
+      }).then(handleResult);
+    },
+    login: function login(params, context) {
+      var request = {
+        redirectUri: context.redirectUri,
+        loginHint: params.loginHint,
+        scopes: scopes
+      };
+      clearInteractionStatus();
+      if (params.interaction === 'popup') {
+        return client.loginPopup(request).then(handleResult);
+      } else {
+        return client.loginRedirect(request);
+      }
+    },
+    logout: function logout(params, context) {
+      var account = client.getAccount({
+        homeAccountId: params.accountId
+      });
+      if (!params.singleLogout) {
+        return client.clearCache({
+          account: account
+        });
+      }
+      clearInteractionStatus();
+      if (params.interaction === 'popup') {
+        return client.logoutPopup({
+          account: account
+        });
+      } else {
+        return client.logoutRedirect({
+          account: account,
+          postLogoutRedirectUri: context.redirectUri
+        });
+      }
+    }
+  };
+}
+util_define(MsalAuthProvider, {
+  defaultConfig: defaultConfig,
+  setDefault: function setDefault(config) {
+    extend(true, defaultConfig, config);
+  },
+  create: function create(key, options, authority, scopes) {
+    if (typeof options === 'string') {
+      options = {
+        scopes: scopes,
+        config: {
+          auth: {
+            clientId: options,
+            authority: authority
+          }
+        }
+      };
+    }
+    var client = is(options, msal_browser_root_msal_.PublicClientApplication) || new msal_browser_root_msal_.PublicClientApplication(extend(true, {}, defaultConfig, options.config));
+    return createProvider(key, client, options);
+  }
+});
+/* harmony default export */ var msal = (MsalAuthProvider);
+}();
+__webpack_exports__ = __webpack_exports__["default"];
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
+//# sourceMappingURL=brew-auth-msal.js.map
---
+++ dist/brew-auth.js
@@ -0,0 +1,511 @@
+/*! @misonou/brew-extension-auth v0.2.0 | (c) misonou | https://misonou.github.io */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("brew-js"), require("zeta-dom"));
+	else if(typeof define === 'function' && define.amd)
+		define("@misonou/brew-extension-auth", ["brew-js", "zeta-dom"], factory);
+	else if(typeof exports === 'object')
+		exports["Auth"] = factory(require("brew-js"), require("zeta-dom"));
+	else
+		root["brew"] = root["brew"] || {}, root["brew"]["Auth"] = factory(root["brew"], root["zeta"]);
+})(self, function(__WEBPACK_EXTERNAL_MODULE__760__, __WEBPACK_EXTERNAL_MODULE__231__) {
+return /******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 760:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__760__;
+
+/***/ }),
+
+/***/ 231:
+/***/ (function(module) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__231__;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+!function() {
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": function() { return /* binding */ entry; }
+});
+
+// NAMESPACE OBJECT: ./src/errorCode.js
+var errorCode_namespaceObject = {};
+__webpack_require__.r(errorCode_namespaceObject);
+__webpack_require__.d(errorCode_namespaceObject, {
+  invalidCredential: function() { return invalidCredential; },
+  loggedIn: function() { return loggedIn; },
+  noProvider: function() { return noProvider; }
+});
+
+// NAMESPACE OBJECT: ./src/index.js
+var src_namespaceObject = {};
+__webpack_require__.r(src_namespaceObject);
+__webpack_require__.d(src_namespaceObject, {
+  AuthError: function() { return errorCode_namespaceObject; },
+  AuthProvider: function() { return provider; },
+  JSONClient: function() { return util_JSONClient; },
+  createAxiosMiddleware: function() { return createAxiosMiddleware; },
+  createFetchMiddleware: function() { return createFetchMiddleware; },
+  "default": function() { return src; }
+});
+
+// EXTERNAL MODULE: external {"commonjs":"brew-js","commonjs2":"brew-js","amd":"brew-js","root":"brew"}
+var external_commonjs_brew_js_commonjs2_brew_js_amd_brew_js_root_brew_ = __webpack_require__(760);
+;// CONCATENATED MODULE: ./|umd|/brew-js/app.js
+
+var addExtension = external_commonjs_brew_js_commonjs2_brew_js_amd_brew_js_root_brew_.addExtension;
+
+;// CONCATENATED MODULE: ./|umd|/brew-js/util/path.js
+
+var combinePath = external_commonjs_brew_js_commonjs2_brew_js_amd_brew_js_root_brew_.combinePath;
+
+// EXTERNAL MODULE: external {"commonjs":"zeta-dom","commonjs2":"zeta-dom","amd":"zeta-dom","root":"zeta"}
+var external_commonjs_zeta_dom_commonjs2_zeta_dom_amd_zeta_dom_root_zeta_ = __webpack_require__(231);
+;// CONCATENATED MODULE: ./|umd|/zeta-dom/util.js
+
+var _lib$util = external_commonjs_zeta_dom_commonjs2_zeta_dom_amd_zeta_dom_root_zeta_.util,
+  catchAsync = _lib$util.catchAsync,
+  defineHiddenProperty = _lib$util.defineHiddenProperty,
+  defineObservableProperty = _lib$util.defineObservableProperty,
+  each = _lib$util.each,
+  errorWithCode = _lib$util.errorWithCode,
+  exclude = _lib$util.exclude,
+  extend = _lib$util.extend,
+  is = _lib$util.is,
+  isFunction = _lib$util.isFunction,
+  makeArray = _lib$util.makeArray,
+  makeAsync = _lib$util.makeAsync,
+  mapRemove = _lib$util.mapRemove,
+  pick = _lib$util.pick,
+  pipe = _lib$util.pipe,
+  reject = _lib$util.reject,
+  resolve = _lib$util.resolve,
+  resolveAll = _lib$util.resolveAll,
+  _throws = _lib$util["throws"];
+
+;// CONCATENATED MODULE: ./|umd|/zeta-dom/dom.js
+
+var reportError = external_commonjs_zeta_dom_commonjs2_zeta_dom_amd_zeta_dom_root_zeta_.dom.reportError;
+
+;// CONCATENATED MODULE: ./src/errorCode.js
+var loggedIn = 'brew/auth-logged-in';
+var noProvider = 'brew/auth-no-provider';
+var invalidCredential = 'brew/auth-invalid-credential';
+;// CONCATENATED MODULE: ./src/extension.js
+
+
+
+
+
+var CACHE_KEY = 'brew.auth';
+/* harmony default export */ var extension = (addExtension('auth', ['router'], function (app, options) {
+  var setUser = defineObservableProperty(app, 'user', null, true);
+  var providers = makeArray(options.providers);
+  var providerParams = {
+    interaction: options.interaction || 'redirect'
+  };
+  var contexts = new Map();
+  var redirectUri = combinePath(location.origin, app.toHref('/'));
+  var sessionCache = app.cache;
+  var currentProvider;
+  var currentResult;
+  function initProvider(provider) {
+    var context = {
+      redirectUri: redirectUri,
+      revokeSession: function revokeSession(accountId) {
+        if (currentProvider === provider && (!accountId || currentResult.accountId === accountId)) {
+          currentProvider = null;
+          if (!app.emit('sessionEnded')) {
+            handleLogout();
+          }
+        }
+      }
+    };
+    contexts.set(provider, context);
+    return resolve(provider.init(context))["catch"](function (e) {
+      reportError(e);
+    });
+  }
+  function findProvider(params) {
+    if (params.provider) {
+      return providers.find(function (v) {
+        return v.key === params.provider;
+      });
+    }
+    if (params.loginHint) {
+      return resolve(0).then(function next(index) {
+        var provider = providers[index];
+        return provider && resolve(provider.isHandleable(params.loginHint)).then(function (v) {
+          return v ? provider : next(index + 1);
+        });
+      });
+    }
+    if (!providers[1]) {
+      return providers[0];
+    }
+  }
+  function setSessionState(provider, returnPath) {
+    sessionCache.set(CACHE_KEY, {
+      provider: provider,
+      returnPath: returnPath
+    });
+  }
+  function popSessionState(provider) {
+    var state = mapRemove(sessionCache, CACHE_KEY) || {};
+    if (state.provider === provider) {
+      catchAsync(app.navigate(state.returnPath));
+    }
+  }
+  function handleLogin(provider, result) {
+    var data = {
+      provider: provider.key,
+      providerType: provider.providerType,
+      account: result.account
+    };
+    currentProvider = provider;
+    currentResult = result;
+    return makeAsync(options.resolveUser)(data).then(function (user) {
+      setUser(user);
+      popSessionState(provider.key);
+      app.emit('login', {
+        user: user
+      });
+    }, function (error) {
+      handleLogout();
+      throw error;
+    });
+  }
+  function handleLogout() {
+    var user = app.user;
+    currentProvider = null;
+    currentResult = null;
+    setUser(null);
+    popSessionState('');
+    if (user) {
+      app.emit('logout', {
+        user: user
+      });
+    }
+  }
+  function callProvider(provider, method, params, callback) {
+    var promise = makeAsync(provider[method]).call(provider, extend({}, providerParams, params), contexts.get(provider));
+    promise["catch"](function () {
+      sessionCache["delete"](CACHE_KEY);
+    });
+    return promise.then(callback);
+  }
+  app.define({
+    resolveAuthProvider: function resolveAuthProvider(params) {
+      return resolve(findProvider(params)).then(function (provider) {
+        return provider ? pick(provider, ['key', 'authType', 'providerType']) : null;
+      });
+    },
+    acquireToken: function acquireToken(callback) {
+      var provider = currentProvider;
+      var result = currentResult;
+      var force = callback === true;
+      callback = isFunction(callback) || resolve;
+      if (!provider) {
+        return callback(null, false);
+      }
+      if (!force && currentResult.expiresOn > Date.now()) {
+        return callback(currentResult.accessToken, true);
+      }
+      return provider.refresh(currentResult, contexts.get(provider)).then(function (result) {
+        if (currentProvider === provider) {
+          currentResult = result;
+        }
+        return callback(result.accessToken, false);
+      }, function (error) {
+        return force ? _throws(error) : callback(result.accessToken, false);
+      });
+    },
+    login: function login(params) {
+      if (currentProvider) {
+        return reject(errorWithCode(loggedIn));
+      }
+      params = params || {};
+      return resolve(findProvider(params)).then(function (provider) {
+        if (!provider) {
+          throw errorWithCode(noProvider);
+        }
+        setSessionState(provider.key, params.returnPath || options.postLoginPath || app.path);
+        return callProvider(provider, 'login', pick(params, ['loginHint', 'password']), handleLogin.bind(0, provider));
+      });
+    },
+    logout: function logout(params) {
+      if (!currentProvider) {
+        return resolve(handleLogout());
+      }
+      params = params || {};
+      setSessionState('', params.returnPath || options.postLogoutPath || app.path);
+      return callProvider(currentProvider, 'logout', {
+        accountId: currentResult.accountId,
+        singleLogout: params.singleLogout
+      }, handleLogout);
+    }
+  });
+  app.beforeInit(function () {
+    return resolveAll(providers.map(initProvider), function (results) {
+      var index = providers.indexOf(findProvider(sessionCache.get(CACHE_KEY) || {}));
+      if (!results[index]) {
+        index = results.findIndex(pipe);
+      }
+      return index >= 0 ? handleLogin(providers[index], results[index]) : handleLogout();
+    });
+  });
+}));
+;// CONCATENATED MODULE: ./|umd|/zeta-dom/domUtil.js
+
+var bind = external_commonjs_zeta_dom_commonjs2_zeta_dom_amd_zeta_dom_root_zeta_.util.bind;
+
+;// CONCATENATED MODULE: ./src/provider.js
+
+
+var AuthProvider = {
+  from: function from(key, client) {
+    var storage = window.localStorage;
+    var storageKey = 'brew.auth.' + key;
+    var storageKeyID = storageKey + '.id';
+    function getCachedData() {
+      try {
+        return JSON.parse(storage[storageKey]);
+      } catch (e) {}
+    }
+    function setCurrentData(data) {
+      if (data) {
+        storage[storageKey] = JSON.stringify(exclude(data, ['account']));
+        storage[storageKeyID] = data.accountId;
+      } else {
+        delete storage[storageKey];
+        delete storage[storageKeyID];
+      }
+      return data;
+    }
+    return {
+      key: key,
+      authType: client.authType,
+      providerType: client.providerType,
+      init: function init(context) {
+        bind(window, 'storage', function (e) {
+          if (e.storageArea === storage && (e.key === storageKeyID && e.oldValue || e.key === null)) {
+            context.revokeSession(e.oldValue);
+          }
+        });
+        var cached = getCachedData();
+        return cached && client.refresh(cached).then(setCurrentData);
+      },
+      login: function login(params) {
+        return client.login(params).then(setCurrentData);
+      },
+      logout: function logout(params) {
+        return client.logout(params).then(setCurrentData.bind(0, undefined));
+      },
+      refresh: function refresh(cached) {
+        return client.refresh(cached).then(setCurrentData);
+      },
+      isHandleable: function isHandleable(loginHint) {
+        return client.isHandleable(loginHint);
+      }
+    };
+  }
+};
+/* harmony default export */ var provider = (AuthProvider);
+;// CONCATENATED MODULE: ./src/middleware.js
+
+function retryOrEnd(acquireToken, response, retryable, callback, onError) {
+  return retryable && response && response.status === 401 ? acquireToken(true).then(callback, onError) : onError();
+}
+function fetchMiddleware(acquireToken, request, next) {
+  if (!is(request, Request) || next && !isFunction(next)) {
+    return fetchMiddleware(acquireToken, new Request(request, next));
+  }
+  next = next || window.fetch;
+  return acquireToken(function execute(accessToken, retryable) {
+    var req = request;
+    if (accessToken) {
+      req = req.clone();
+      req.headers.set('authorization', 'Bearer ' + accessToken);
+    }
+    return next(req).then(function (response) {
+      return retryOrEnd(acquireToken, response, retryable, execute, function () {
+        return response;
+      });
+    });
+  });
+}
+function axiosMiddleware(acquireToken, axios) {
+  var handled = new WeakMap();
+  var withBearerToken = function withBearerToken(config, accessToken) {
+    config.headers.authorization = 'Bearer ' + accessToken;
+    return config;
+  };
+  axios.interceptors.request.use(function (config) {
+    if (mapRemove(handled, config)) {
+      // refreshed access token is already set in error interceptors
+      // flag is removed from map to avoid recursion
+      return config;
+    }
+    return acquireToken(function (accessToken, retryable) {
+      handled.set(config, retryable);
+      return accessToken ? withBearerToken(config, accessToken) : config;
+    });
+  });
+  axios.interceptors.response.use(undefined, function (error) {
+    error = error || {};
+    return retryOrEnd(acquireToken, error.response, handled.get(error.config), function (accessToken) {
+      return axios(withBearerToken(error.config, accessToken));
+    }, function () {
+      return reject(error);
+    });
+  });
+  return axios;
+}
+function createFetchMiddleware(app) {
+  return fetchMiddleware.bind(0, app.acquireToken);
+}
+function createAxiosMiddleware(app) {
+  return axiosMiddleware.bind(0, app.acquireToken);
+}
+;// CONCATENATED MODULE: ./|umd|/brew-js/errorCode.js
+
+var errorCode = external_commonjs_brew_js_commonjs2_brew_js_amd_brew_js_root_brew_.ErrorCode;
+var apiError = errorCode.apiError;
+
+;// CONCATENATED MODULE: ./src/util/JSONClient.js
+
+
+
+function getOptionsWithBody(method, body, options) {
+  options = options || {};
+  return extend({}, options, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: extend({}, options.headers, {
+      'content-type': 'application/json'
+    })
+  });
+}
+function fetchJSON(request, next) {
+  request.headers.set('accept', 'application/json');
+  return (next || fetch)(request).then(function (response) {
+    return response.json().then(function (data) {
+      if (response.ok) {
+        return data;
+      }
+      throw errorWithCode(apiError, '', {
+        data: data
+      });
+    }, function (e) {
+      if (response.ok) {
+        // assume empty for non-JSON response with 2xx status
+        return null;
+      }
+      throw e;
+    });
+  });
+}
+function JSONClient(baseUrl, middleware) {
+  middleware = middleware || function (request, next) {
+    return next(request);
+  };
+  this.baseUrl = baseUrl;
+  this.request = function (request, options) {
+    request = is(request, Request) || new Request(combinePath(baseUrl, request), options);
+    return middleware(request, fetchJSON);
+  };
+}
+each('get post put patch delete', function (i, v) {
+  var method = v.toUpperCase();
+  defineHiddenProperty(JSONClient.prototype, v, function (path, body, options) {
+    return this.request(path, v === 'get' ? body : getOptionsWithBody(method, body, options));
+  });
+});
+/* harmony default export */ var util_JSONClient = (JSONClient);
+;// CONCATENATED MODULE: ./src/util.js
+
+
+;// CONCATENATED MODULE: ./src/index.js
+
+
+
+/* harmony default export */ var src = (extension);
+
+
+
+;// CONCATENATED MODULE: ./src/entry.js
+
+
+Object.assign(src, src_namespaceObject);
+/* harmony default export */ var entry = (src);
+}();
+__webpack_exports__ = __webpack_exports__["default"];
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
+//# sourceMappingURL=brew-auth.js.map
