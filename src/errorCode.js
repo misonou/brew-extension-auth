3b78f779af45f9c7da2bfa37b51fd759ca23c992
Takeru <takeru.moe@gmail.com>
2026-05-17 23:13:05 +0800

    fix: erroneously added declaration 314c48f

--- src/errorCode.js
+++ src/errorCode.js
@@ -1,5 +1,6 @@
 export const loggedIn = 'brew/auth-logged-in';
 export const noProvider = 'brew/auth-no-provider';
 export const userNotLoggedIn = 'brew/auth-user-not-logged-in';
+export const passKeyUnavailable = 'brew/auth-passkey-unavailable';
 export const missingCredential = 'brew/auth-missing-credential';
 export const invalidCredential = 'brew/auth-invalid-credential';
